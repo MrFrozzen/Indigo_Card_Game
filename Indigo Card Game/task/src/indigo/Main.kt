1abe855ea2d63509c0091390d2b7ce82ac1149fc
MrFrozzen <mrfrost2035@gmail.com>
2022-03-28 23:18:34 +0300

    Initial commit

---
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -0,0 +1,10 @@
+package indigo
+
+fun main() {
+    val ranks = "A 2 3 4 5 6 7 8 9 10 J Q K"
+    val suits = "♦ ♥ ♠ ♣"
+    val deck = "A♠ 2♠ 3♠ 4♠ 5♠ 6♠ 7♠ 8♠ 9♠ 10♠ J♠ Q♠ K♠ A♥ 2♥ 3♥ 4♥ 5♥ 6♥ 7♥ 8♥ 9♥ 10♥ J♥ Q♥ K♥ A♦ 2♦ 3♦ 4♦ 5♦ 6♦ 7♦ 8♦ 9♦ 10♦ J♦ Q♦ K♦ A♣ 2♣ 3♣ 4♣ 5♣ 6♣ 7♣ 8♣ 9♣ 10♣ J♣ Q♣ K♣"
+    println(ranks)
+    println(suits)
+    println(deck)
+}
